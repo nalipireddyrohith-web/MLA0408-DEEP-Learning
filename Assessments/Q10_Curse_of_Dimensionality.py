c205f8b4b92da65fadc5e1ad7daf873917e6f9fd
nalipireddyrohith-web <nalipireddyrohith@gmail.com>
2026-07-23 14:37:22 +0530

    Delete Assessments/Q10_Curse_of_Dimensionality.py

--- Assessments/Q10_Curse_of_Dimensionality.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-for d in [2,10,50,100,500]:
-    X=np.random.rand(100,d)
-    dist=[]
-    for i in range(99):
-        dist.append(np.linalg.norm(X[i]-X[i+1]))
-    print("Dimension:",d)
-    print("Average Distance:",np.mean(dist))
