47d538e77a167950c6d1a1de25017859a92e88d2
nalipireddyrohith-web <nalipireddyrohith@gmail.com>
2026-08-19 11:21:36 +0530

    Delete Assessments/CO1_AT3 EXPERIMENTS directory

--- Assessments/CO1_AT3 EXPERIMENTS/Q9_MiniBatch_GD.py
+++
@@ -1,8 +0,0 @@
-from sklearn.neural_network import MLPRegressor
-import numpy as np
-
-X=np.random.rand(100,2)
-y=X[:,0]+X[:,1]
-model=MLPRegressor(batch_size=16,max_iter=500,random_state=42)
-model.fit(X,y)
-print("Score:",model.score(X,y))
